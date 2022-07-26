682104bd007173e892487dff9d758c3ec5551868
Christian Kothe <christiankothe@gmail.com>
2022-07-26 14:39:06 -0700

    using modded (PEP440 compliant) version for disambiguation with upstream

--- pynamodb/__init__.py
+++ pynamodb/__init__.py
@@ -7,4 +7,4 @@ A simple abstraction over DynamoDB
 """
 __author__ = 'Jharrod LaFon'
 __license__ = 'MIT'
-__version__ = '5.0.3-rb'  # modded version here for own use
+__version__ = '5.0.3.dev1'  # modded version here for own use
